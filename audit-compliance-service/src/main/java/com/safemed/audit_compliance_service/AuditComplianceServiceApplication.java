1d9a5bf296bb59d4adb5c88df8df0069dfa45124
Sudenaz Kaplan <sudenazkaplan39@gmail.com>
2026-06-07 15:32:42 +0300

    chore: initialize audit service infrastructure and configure isolated postgres database

---
+++ audit-compliance-service/src/main/java/com/safemed/audit_compliance_service/AuditComplianceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.safemed.audit_compliance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditComplianceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditComplianceServiceApplication.class, args);
+	}
+
+}
